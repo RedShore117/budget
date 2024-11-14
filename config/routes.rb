c64f7c4fd3ddc80d62b807c8bae661c7ae4639dc
RedShore117 <fullgr1m@ya.ru>
2024-11-14 12:55:54 +0300

    add home controller and generate authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get "home/index"
-  get "home/dashboard"
+  resource :session
+  resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+  get "home/dashboard", as: :dashboard
 end
